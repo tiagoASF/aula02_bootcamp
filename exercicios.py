382cdc126e462c18c4963192199aeb4101ee11bf
Tiago Fialho <tiago.chem@gmail.com>
2024-12-20 06:12:54 -0300

    exercicio 6 resolvido

--- exercicios.py
+++ exercicios.py
@@ -24,10 +24,29 @@
 # print(f"O resultado da divisão inteira de {valor1} por {valor2} é {resultado}")
 
 # 5 - Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
-import math
+# import math
 
-valor = float(input("Informe o valor desejado: "))
-resultado = math.sqrt(valor)
-print(f"A raiz quadrada de {valor} é {resultado:.2f}")
+# valor = float(input("Informe o valor desejado: "))
+# resultado = math.sqrt(valor)
+# print(f"A raiz quadrada de {valor} é {resultado:.2f}")
+
+#PONTO FLUTUANTE
+#6 - Escreva um programa que receba dois números flutuantes e realize sua adição.
+
+valor1 = float(input("Informe o primeiro valor: "))
+valor2 = float(input("Informe o segundo valor: "))
+
+resultado = valor1 + valor2
+
+print(f"A soma entre {valor1} e {valor2} é {resultado:.2f}")
+
+
+# 7 - Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+
+# 8 - Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+
+# 9 - Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+
+# 10 - Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
 
 
