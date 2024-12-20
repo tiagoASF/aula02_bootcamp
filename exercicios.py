5f3f9ac0c7c2d30d6e854ae257a66bdc657e5e6e
Tiago Fialho <tiago.chem@gmail.com>
2024-12-20 07:24:00 -0300

    exercicio 18 resolvido

--- exercicios.py
+++ exercicios.py
@@ -112,14 +112,16 @@
 
 
 # 17 - Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
-valor1 = False
-valor2 = True
-resultado_final = valor1 or valor2
-print(resultado_final)  
+# valor1 = False
+# valor2 = True
+# resultado_final = valor1 or valor2
+# print(resultado_final)  
 
 
 # 18 - Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
-
+valor1 = True
+resultado = not valor1
+print(resultado)
 # 19 - Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
 
 # 20 - Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
