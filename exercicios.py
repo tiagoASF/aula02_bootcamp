c59857add9f14bea059d8c7026529c298e888e9a
Tiago Fialho <tiago.chem@gmail.com>
2024-12-20 07:07:09 -0300

    exercicio 14 resolvido

--- exercicios.py
+++ exercicios.py
@@ -87,11 +87,15 @@
 # print(nome_tratado)
 
 # 13 - Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
-frase = input("Insira uma frase: ")
-frase_tratado = frase.strip()
-print(frase_tratado)
+# frase = input("Insira uma frase: ")
+# frase_tratado = frase.strip()
+# print(frase_tratado)
 
 # 14 - Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+data_string = input("Informe a data no formato dd/mm/aaaa: ")
+dia, mes, ano = data_string.split("/")
+print(f"Dia:{dia}, Mês:{mes}, Ano:{ano}")
+
 
 # 15 - Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
