b6d4a69ab580d7e2ade974158dca603a2fc80c5e
Tiago Fialho <tiago.chem@gmail.com>
2024-12-20 06:37:46 -0300

    exercicio 11 resolvido

--- exercicios.py
+++ exercicios.py
@@ -65,12 +65,27 @@
 # print(f"{temperatura_c}oC em Fahreheint é {temperatura_f}")
 
 # 10 - Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
-import math
+# import math
 
-PI = math.pi
-raio = float(input("Qual o raio do círculo: "))
+# PI = math.pi
+# raio = float(input("Qual o raio do círculo: "))
 
-area_circulo = PI * math.pow(raio,2)
+# area_circulo = PI * math.pow(raio,2)
 
-print(f"A área do circulo de raio {raio} é {area_circulo:.2f}")
+# print(f"A área do circulo de raio {raio} é {area_circulo:.2f}")
+
+#Strings (str)
+# 11 - Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+palavra = input("Informe o nome a ser convertido: ").strip().lower()
+print(palavra.upper())
+
+# 12 - Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+
+# 13 - Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+
+# 14 - Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+
+# 15 - Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
 
