a3f9f8d5a011ee09db15a62794df5f8611fa3d10
Tiago Fialho <tiago.chem@gmail.com>
2024-12-20 05:54:08 -0300

    exercicio 2 resolvido

--- exercicios.py
+++ exercicios.py
@@ -1,11 +1,15 @@
 # INT
 #1- Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-valor1 = int(input("Informe o primeiro valor: "))
-valor2 = int(input("Informe o segundo valor: "))
-soma = valor1 + valor2
-print(f"A soma de {valor1} e {valor2} é {soma}")
+# valor1 = int(input("Informe o primeiro valor: "))
+# valor2 = int(input("Informe o segundo valor: "))
+# soma = valor1 + valor2
+# print(f"A soma de {valor1} e {valor2} é {soma}")
 
 # 2 - Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+DIVIDENDO = 5
+valor_usuario = float(input("Informe um valor: "))
+resto_divisao = valor_usuario % DIVIDENDO
+print(f"O resto da divisão entre {valor_usuario} e {DIVIDENDO} é {resto_divisao:.0f}")
 
 # 3 - Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 
