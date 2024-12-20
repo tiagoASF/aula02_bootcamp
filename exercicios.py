3ecce25650fe6b8f0cb68081caa7952e4d0f556a
Tiago Fialho <tiago@mymacbook.local>
2024-12-20 05:49:21 -0300

    exercicio 1 resolvido

---
+++ exercicios.py
@@ -0,0 +1,15 @@
+# INT
+#1- Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+valor1 = int(input("Informe o primeiro valor: "))
+valor2 = int(input("Informe o segundo valor: "))
+soma = valor1 + valor2
+print(f"A soma de {valor1} e {valor2} é {soma}")
+
+# 2 - Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+# 3 - Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+# 4 - Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+# 5 - Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
