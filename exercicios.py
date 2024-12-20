39b846a18b6178e9d81af3c7db306d10cdc76b4a
Tiago Fialho <tiago.chem@gmail.com>
2024-12-20 05:56:32 -0300

    exercicio 3 resolvido

--- exercicios.py
+++ exercicios.py
@@ -6,12 +6,16 @@
 # print(f"A soma de {valor1} e {valor2} é {soma}")
 
 # 2 - Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
-DIVIDENDO = 5
-valor_usuario = float(input("Informe um valor: "))
-resto_divisao = valor_usuario % DIVIDENDO
-print(f"O resto da divisão entre {valor_usuario} e {DIVIDENDO} é {resto_divisao:.0f}")
+# DIVIDENDO = 5
+# valor_usuario = float(input("Informe um valor: "))
+# resto_divisao = valor_usuario % DIVIDENDO
+# print(f"O resto da divisão entre {valor_usuario} e {DIVIDENDO} é {resto_divisao:.0f}")
 
 # 3 - Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+valor1 = float(input("Informe o primeiro valor: "))
+valor2 = float(input("Informe o segundo valor: "))
+resultado_multiplicacao = valor1 * valor2
+print(f"O resultado da multiplicação é {resultado_multiplicacao}")
 
 # 4 - Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 
