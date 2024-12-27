c1219e24aa2d8b9d5ac306b1815e0d984e487105
Tiago Fialho <tiago.chem@gmail.com>
2024-12-27 06:08:51 -0300

    Exercicio 24 resolvido

--- exercicios.py
+++ exercicios.py
@@ -165,41 +165,59 @@
 # Exercício 23: Calculadora Simples
 # Desenvolva uma calculadora simples que aceite duas entradas numéricas e um operador (+, -, *, /) do usuário. Use try-except para lidar com divisões por zero e entradas não numéricas. Utilize if-elif-else para realizar a operação matemática baseada no operador fornecido. Imprima o resultado ou uma mensagem de erro apropriada.
 
-try:
-    valor1 = float(input("Informe o primeiro valor: "))
-except ValueError as e:
-    print("O valor digitado não é numérico")
+# try:
+#     valor1 = float(input("Informe o primeiro valor: "))
+# except ValueError as e:
+#     print("O valor digitado não é numérico")
 
-try:
-    valor2 = float(input("Informe o segundo valor: "))
-except ValueError as e:
-    print("O valor digitado não é numérico")
+# try:
+#     valor2 = float(input("Informe o segundo valor: "))
+# except ValueError as e:
+#     print("O valor digitado não é numérico")
 
-try:
-    operacao = input("Qual operação(+, -, *, /) deseja realizar: ").strip()
-except TypeError as e:
-    print("O valor digitado não é uma string válida")
+# try:
+#     operacao = input("Qual operação(+, -, *, /) deseja realizar: ").strip()
+# except TypeError as e:
+#     print("O valor digitado não é uma string válida")
 
-if operacao == "+":
-    print(f"A soma entre {valor1} e {valor2} é {valor1 + valor2}")
-elif operacao == "-":
-    print(f"A subtração de {valor1} por {valor2} é {valor1 - valor2}")
-elif operacao == "*":
-    print(f"A multiplicação  entre {valor1} e {valor2} é {valor1 * valor2}")
-elif operacao == "/":
-    try:
-        print(f"A divisão de {valor1} por {valor2} é {valor1 / valor2}")
-    except ZeroDivisionError as e:
-        print("A divisão por zero não é válida")
+# if operacao == "+":
+#     print(f"A soma entre {valor1} e {valor2} é {valor1 + valor2}")
+# elif operacao == "-":
+#     print(f"A subtração de {valor1} por {valor2} é {valor1 - valor2}")
+# elif operacao == "*":
+#     print(f"A multiplicação  entre {valor1} e {valor2} é {valor1 * valor2}")
+# elif operacao == "/":
+#     try:
+#         print(f"A divisão de {valor1} por {valor2} é {valor1 / valor2}")
+#     except ZeroDivisionError as e:
+#         print("A divisão por zero não é válida")
     
-else:
-    print(f"{operacao} não é uma operação válida")
+# else:
+#     print(f"{operacao} não é uma operação válida")
 
 
 
 
 # Exercício 24: Classificador de Números
 # Escreva um programa que solicite ao usuário para digitar um número. Utilize try-except para assegurar que a entrada seja numérica e utilize if-elif-else para classificar o número como "positivo", "negativo" ou "zero". Adicionalmente, identifique se o número é "par" ou "ímpar".
+
+
+try:
+    valor = int(input("Informe o valor inteiro desejado: "))
+    positivo_negativo = "positivo"
+    par_impar = "par"
+
+    if valor % 2 != 0:
+        par_impar = "ímpar"
+
+    if valor < 0:
+        positivo_negativo = "negativo"
+    elif valor == 0:
+        positivo_negativo = "zero"
+    
+    print(f"O valor {valor} é {positivo_negativo} e {par_impar}")
+except ValueError as e:
+    print("O valor informado deve ser numérico")
 
 # Exercício 25: Conversão de Tipo com Validação
 # Crie um script que solicite ao usuário uma lista de números separados por vírgula. O programa deve converter a string de entrada em uma lista de números inteiros. Utilize try-except para tratar a conversão de cada número e validar que cada elemento da lista convertida é um inteiro. Se a conversão falhar ou um elemento não for um inteiro, imprima uma mensagem de erro. Se a conversão for bem-sucedida para todos os elementos, imprima a lista de inteiros.
